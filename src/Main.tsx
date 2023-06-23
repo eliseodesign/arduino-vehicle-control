f333bd68ae7ab32a7af03c0ff65c3eef648a9b99
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-06-22 21:06:53 -0600

    [:sparkles: feat] add config to repositorie

---
+++ src/Main.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View, Text } from 'react-native'
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Arduino Vehicle Control v1.0.0</Text>
+    </View>
+  )
+}
+export default Main
+
+const styles = StyleSheet.create({
+  main:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+
